f1e432d66f76a1a861f000b2156aaf336317e925
Chris Whinfrey <cwhinfrey@gmail.com>
2018-08-23 00:32:56 -0700

    Switch rpc ports

--- truffle.js
+++ truffle.js
@@ -5,13 +5,13 @@ module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 9545,
+      port: 8546,
       network_id: '*', // Match any network id
       gas: 6000000
     },
     kovan: {
       host: 'localhost',
-      port: 9545,
+      port: 8545,
       network_id: '*', // Match any network id
       gas: 6000000
     },
